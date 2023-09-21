53855903010e0b59eaca41b360456235cf6ac2cd
OngkyRinaldo <edoorinaldoo@gmail.com>
2023-09-21 07:41:48 +0700

    update layout inbox modal

--- src/components/search/SearchModal.jsx
+++ src/components/search/SearchModal.jsx
@@ -6,12 +6,12 @@ const SearchModal = ({ searchUser, setSearchUser }) => {
             <input
                 type='text'
                 placeholder='Search...'
-                className='border border-gray-300  py-2 pr-10 pl-16 rounded-md focus:outline-none w-full placeholder:text-main '
+                className='border border-gray-300  py-2 pr-10 pl-3 lg:pl-16 rounded-md focus:outline-none w-full placeholder:text-main '
                 onChange={(e) => setSearchUser(e.target.value)}
                 value={searchUser}
             />
 
-            <div className='absolute inset-y-0 right-5 pr-3 flex items-center pointer-events-none'>
+            <div className='absolute inset-y-0 right-5 lg:pr-3 flex items-center pointer-events-none'>
                 <img src={searchModalIcon} alt='searchModalIcon.svg' />
             </div>
         </div>
