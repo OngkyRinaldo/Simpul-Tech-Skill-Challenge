5f9672de5733bfcbaf09f6614645786ea505448e
OngkyRinaldo <edoorinaldoo@gmail.com>
2023-09-22 11:11:41 +0700

    update

--- src/components/search/SearchHomePage.jsx
+++ src/components/search/SearchHomePage.jsx
@@ -1,4 +1,4 @@
-import SearchHomePageButton from '../../assets/icons/buttons/searchHomePage';
+import searchHomePageIcon from '../../assets/icons/Buttons/searchHomePage.svg';
 
 const SearchHomePage = () => {
     return (
@@ -8,7 +8,7 @@ const SearchHomePage = () => {
                 className='border-none focus:outline-none  bg-primary-live w-full  h-[58px]'
             />
             <div className='absolute inset-y-0 flex items-center pointer-events-none pl-paddingLeftHomeSearch pt-paddingTopHomeSearch pb-paddingBottomHomeSearch'>
-                <SearchHomePageButton />
+                <img src={searchHomePageIcon} alt='searchHomePage.svg' />
             </div>
         </section>
     );
