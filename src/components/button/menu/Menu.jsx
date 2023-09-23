f062d77c3300f7a5efe393610c1c1fadd4279ca5
OngkyRinaldo <edoorinaldoo@gmail.com>
2023-09-23 09:37:02 +0700

    update title app

--- src/components/button/menu/Menu.jsx
+++ src/components/button/menu/Menu.jsx
@@ -5,23 +5,23 @@ import menuIcon from '../../../assets/icons/buttons/menuIcon.svg';
 
 const Menu = () => {
     const [showButtons, setShowButtons] = useState(false);
-
     const handleButtonClick = () => {
         setShowButtons(!showButtons);
     };
 
     return (
-        <div className='flex justify-center items-center gap-x-8 absolute bottom-[33px] right-10'>
+        <div className='flex justify-center items-center gap-x-8 absolute bottom-[33px] right-10 '>
             {showButtons && (
                 <div className='flex justify-center items-center gap-x-8'>
                     <Task />
+
                     <Inbox />
                 </div>
             )}
 
             <button
-                className={`bg-primary-blue text-white w-12 h-12 xl:w-16 xl:h-16 flex items-center justify-center p-2 rounded-full mt-${
-                    showButtons ? '10' : '0'
+                className={`bg-primary-blue text-white w-12 h-12 xl:w-16 xl:h-16 flex items-center justify-center p-2 rounded-full ${
+                    showButtons ? 'mt-10' : 'mt-0'
                 }`}
                 onClick={handleButtonClick}
             >
